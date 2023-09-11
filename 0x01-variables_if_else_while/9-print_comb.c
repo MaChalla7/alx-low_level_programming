9eafec36e17871f721169d097a290e5a20503158
MaChalla7 <emmanuelafaha07@gmail.com>
2023-09-11 02:20:48 +0100

    commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,17 +9,15 @@
 
 int main(void)
 {
-
-        int d;
-        for (d = '0' ; d <= '9' ; d++)
-        {
-        putchar(d);
-        if (d != '9')
-        {
-        putchar(',');
-        putchar(',');
-        }
-        }
-        putchar('\n');
-        return (0);
-}
+  int d;
+  for (d = '0' ; d <= '9' ; d++)
+  {
+  putchar(d);
+  if (d != '9')
+  {
+  putchar(',');
+  }
+  }
+  putchar('\n');
+  return (0);
+}  
