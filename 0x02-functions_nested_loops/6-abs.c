33572048adb7f280c27aa23f90d960861c749747
MaChalla7 <emmanuelafaha07@gmail.com>
2023-09-13 00:09:59 +0100

    6 abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - shows the absolute value of an integer
+ * @j: the integer to be computed
+ * Return: absolute value
+ */
+
+int _abs(int j)
+{
+if (j >= 0)
+{
+return (j);
+}
+else
+{
+return (-j);
+}
+}
