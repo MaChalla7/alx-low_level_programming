2a2877940deeebb2b873353e89045f9e9d5328c7
MaChalla7 <emmanuelafaha07@gmail.com>
2023-09-13 00:13:48 +0100

    ls

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_last_digit - prints the last digit
+* @n: the number
+* return: value of the last digits(id)
+*/
+
+int print_last_digit(int n)
+{
+int id = n % 10;
+if (id < 0)
+id *= -1;
+_putchar(id + '0');
+return (0);
+}
