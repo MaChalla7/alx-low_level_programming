1d604660b4e86108fb578a1c9ecc6312ef821203
MaChalla7 <emmanuelafaha07#gmail.com>
2023-09-19 10:47:00 +0000

    commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
-* _strien - returns the length of a string
-* @s: string
-* Return: length
-*/
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
 int _strlen(char *s)
 {
-	int longi = 0;
-	
-	while (*s != '\0')
-	{
-	longi++;
-	s++;
-	}
-	
-	return (longi);
+int strlen = 0;
+
+while (*s != '\0')
+{
+strlen++;
+s++;
+}
+
+return (strlen);
 }
