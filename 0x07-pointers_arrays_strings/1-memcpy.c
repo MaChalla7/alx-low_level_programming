523c8e1904e67223854f6dae05679717fb0abd14
MaChalla7 <emmanuelafaha07#gmail.com>
2023-09-26 08:54:51 +0000

    commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
-* *memcpy - copies memory area from src to dest
-* @dest: destination
-* @src: source
-* @n: max bytes to use
-* Return: dest
-*/
+ * *_memcpy - copies memory area from src to dest
+ * @dest: destination
+ * @src: source
+ * @n: max bytes to use
+ * Return: dest
+ */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
-
-for (i = 0; n > 0; i++, n--)
+for (i = 0; n > 0 ; i++, n--)
 {
 dest[i] = src[i];
 }
