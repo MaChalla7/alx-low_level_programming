baab2486584da60e8459403060085238912a436d
MaChalla7 <emmanuelafaha07@gmail.com>
2023-10-20 01:02:17 +0100

    updated

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,20 +1,20 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
  * free_list - free a Linked List
  * @head: list_t List to be Free
+ * Return: no Return.
  */
 void free_list(list_t *head)
 {
-list_t *temp;
+list_t *current;
 
-while (head)
+while ((current = head) != NUL)
 {
 temp = head->next;
 free(head->str);
 free(head);
-head = temp;
+head = current;
 }
 }
 
